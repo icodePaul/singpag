a682b66fe0cdb21dc1cc7abe3b31613fb7b153f8
icodePaul <paulokehie817@gmail.com>
2023-09-20 05:54:06 -0700

    commiting all files

---
+++ app.js
@@ -0,0 +1,118 @@
+// const speark = function(name, age, country) {
+
+//     let personDetails = ` My name is :${name} and my age is: ${age} and i am from: ${country}`
+//     personDetails.name = 'john';
+//     personDetails.age = 32,
+//     personDetails.country = 'United State';
+
+//     if(personDetails.name === 'john' || personDetails.age === 32 || personDetails.country === 'United State') {
+
+//         console.log("You entered the right information")
+//     }else {
+//         console.log('The information you entered is not correct')
+//     }
+//     return personDetails
+// };
+
+// console.log(speark('john', 32, 'United State'));
+
+// let calVal = function () {
+//     return area
+// };
+// finding the area of a ritangle
+// let calaculate = function (radius) {
+//     let area = 3.21 * radius**2
+//     return area
+// };
+// const areResult = calaculate(4)
+// console.log(areResult)
+
+
+
+// const val = function (areResult) {
+//     radial = 34.2 * calaculate(areResult);
+//     console.log(radial)
+//     return radial
+// }
+
+// const result = val(areResult)
+// console.log(result)
+
+// const materAarray = () => {
+
+
+// }
+
+// 
+
+
+// const area = 39.7
+// The round method rounds up a number to the nearest integer either up or down pending
+// console.log(Math.round(area));
+// The floor method floors a number to the first interger no mater d decimal number
+// console.log(Math.floor(area))
+// The ceil method ceil up an interger, to add up a number
+// console.log(Math.ceil(area))
+// The trunc method just remove the decimal and the number after the decimal
+// console.log(Math.trunc(area));
+// const random = Math.random()
+// // console.log(random)
+// console.log(Math.round(random * 100))
+
+// let scoreOne = 50;
+
+// let scoreTwo = scoreOne
+
+// // scoreOne = 100;
+
+// console.log(`scoreOne is: ${scoreOne} and scoreTwo is: ${scoreTwo}`);
+
+// const userOne = {
+//     name: 'promise',
+//     age: 39,
+// }
+// const userTwo = userOne
+// const paras = document.querySelectorAll('p')
+// paras.forEach(para => {
+//     // console.log(para.innerText)
+//     para.innerText += ' I am here to make changes';
+
+
+// })
+
+
+
+
+// let products = ['tShirt', 'shoes', 'cars', 'machines', 'bike'];
+
+
+// let content = document.querySelector(".content");
+// let total = 0;
+// for(let i = 0; i < products.length; i++) {
+// total += products[i] 
+
+
+
+//  if(content.innerHTML += `<p>${products[i]}</p>`) {
+// let paras = document.querySelectorAll("div.content");
+// paras.innerText = content[i]
+
+// paras.forEach((para) => {
+// para.style.color = content.innerHTML.backgroundColor = "green";
+  
+// });
+//  }else if(paras)
+ 
+//  }
+
+const getId = document.querySelector('.form-form')
+console.log(getId.getAttribute('class'))
+
+
+ 
+ 
+ 
+
+
+
+
